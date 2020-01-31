6afa8452e3ea88e8bc17a7175d02a63aa3a124f8
Michael Polidori <spatiumestabsolutum@gmail.com>
2020-01-31 08:48:23 -0500

    jfkdsfjksdfj

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -7,6 +7,9 @@ echo "Installing the packages that CKAN requires..."
 sudo apt-get update -qq
 sudo apt-get install solr-jetty
 
+ls
+ls ckan/ckan/config
+
 echo "Setting up Solr..."
 echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
 sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
